85affd75e4f2f81d27c25c9aee0d30f6ed8d595f
Natan Simoes <natan16simoes@gmail.com>
2024-06-01 23:16:17 +0100

    updade cors

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const errorHandler = require("./middleware/error-handler");
 
 const app = express();
 
-// MIDDLEWARE
+// Middleware
 app.use(logger("dev"));
 app.use(express.static("public"));
 app.use(express.json());
+app.use(cors()); // Adicione esta linha para habilitar CORS
 
 // Conectar ao MongoDB
 mongoose
@@ -19,19 +21,19 @@ mongoose
   )
   .catch((err) => console.error("Error connecting to mongo", err));
 
-// IMPORTAR ROTAS
+// Importar rotas
 const authRoutes = require("./routes/auth.routes");
 const commentRoutes = require("./routes/comment.routes");
 const themeRoutes = require("./routes/theme.routes");
 const activeThemeRoutes = require("./routes/active-theme.routes");
 
-// USAR ROTAS
+// Usar rotas
 app.use("/auth", authRoutes);
 app.use("/comments", commentRoutes);
 app.use("/themes", themeRoutes);
-app.use("/active-themes", activeThemeRoutes);
+app.use("/active-themes", activeThemeRoutes); // Certifique-se de que esta linha está correta
 
-// ROTA PRINCIPAL
+// Rota principal
 app.get("/", (req, res) => {
   res.send("Welcome to the backend server!");
 });
